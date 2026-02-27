f76a9952b4588c5cf71e4a5905df340a924cd3d0
JohnMalugu <johnmalugu99@gmail.com>
2026-02-27 15:13:13 +0300

    Initial Commit

---
+++ SokoMkononiTests/SokoMkononiTests.swift
@@ -0,0 +1,17 @@
+//
+//  SokoMkononiTests.swift
+//  SokoMkononiTests
+//
+//  Created by John Malugu on 27/02/2026.
+//
+
+import Testing
+@testable import SokoMkononi
+
+struct SokoMkononiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
